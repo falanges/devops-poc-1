e4ec5f08916fdd99849c3d4afcef374b1dcea343
Matias Parache <matiasparache@pm.me>
2023-10-06 15:43:13 -0300

    + Added jest on each app, updated turbo pipeline

---
+++ apps/docs/app/sum.tsx
@@ -0,0 +1,9 @@
+// This file is only to demonstrate the right functionality of jest triggering the tests
+
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function mul(x: number, y: number): number {
+  return x * y;
+}
